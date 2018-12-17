8763f206055659f557535050c362e0ff5b9da8d5
MattBrozusky <matthew.brozusky@gmail.com>
2018-12-17 11:06:22 -0600

    added migration and seeder

--- migration.sql
+++ migration.sql
@@ -1,22 +1,36 @@
-USE adlister_db;
+USE adlister;
 
-DROP TABLE IF EXISTS ads;
-DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
+DROP TABLE IF EXISTS category;
+
+CREATE TABLE user (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username VARCHAR(240) NOT NULL,
-    email VARCHAR(240) NOT NULL,
+    username VARCHAR(240) UNIQUE NOT NULL,
+    email VARCHAR(240) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE ads (
+CREATE TABLE ad (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    picture VARCHAR(255),
+    title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-        ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES user (id)
 );
+
+# CREATE TABLE ad_category (
+#     ad_id INTEGER UNSIGNED NOT NULL,
+#     cat_id INTEGER UNSIGNED NOT NULL,
+#     FOREIGN KEY (ad_id) REFERENCES ad (id),
+#     FOREIGN KEY (cat_id) REFERENCES category(id)
+# );
+
+# CREATE TABLE category (
+#     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#     name VARCHAR(255) NOT NULL,
+#     PRIMARY KEY (id)
+# );
---
+++ seeder.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO user (username, email, password) VALUES
+      ('justin','justin.reich@gmail.com', 'password1'),
+      ('matt', 'joe.blow@gmail.com', 'password2'),
+      ('pibo', 'maximus@gmail.com', 'password3');
+
+INSERT INTO ad (title, description, user_id, category) VALUES
+      ('Cats food', 'Nutritious food for cats', 4, 'space'),
+      ('Eye shadow', 'Best makeup ever', 5, 'space'),
+      ('CAR parts for sale', 'Best parts for your vehicle!', 6, 'space'),
+      ('Dog toys', 'Make your dog happy', 6, 'space'),
+      ('Lipstick', 'Best shades of colors', 4, 'space'),
+      ('Looking for hot singles?', 'Lonely, call 1-888-400-5555', 5, 'space');
+
+
+select * from ad;
