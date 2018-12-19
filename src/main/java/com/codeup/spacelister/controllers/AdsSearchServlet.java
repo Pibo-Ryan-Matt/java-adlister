e967fb6276ad3d0768d2e1ee31da5a2ea70a0414
MattBrozusky <matthew.brozusky@gmail.com>
2018-12-19 16:33:30 -0600

    finished search btns

--- src/main/java/com/codeup/spacelister/controllers/AdsSearchServlet.java
+++ src/main/java/com/codeup/spacelister/controllers/AdsSearchServlet.java
@@ -14,11 +14,23 @@ public class AdsSearchServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().setAttribute("searchLocation",  req.getParameter("searchLocation"));
+        int searchLocation = (int) req.getSession().getAttribute("searchLocation");
+
+
         req.getSession().setAttribute("searchTerm",  req.getParameter("search"));
         String searchTerm = (String) req.getSession().getAttribute("searchTerm");
 
-        req.setAttribute("ads", DaoFactory.getAdsDao().search(searchTerm, 1));
+        if (searchLocation == 1){
+            req.setAttribute("ads", DaoFactory.getAdsDao().search(searchTerm, 1));
+
+        } else {
+            req.setAttribute("ads", DaoFactory.getAdsDao().search(searchTerm, 1));
+        }
+
+
         req.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(req, resp);
+
 
     }
 }
